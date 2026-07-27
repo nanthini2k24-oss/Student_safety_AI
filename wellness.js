18fb5b79066226704e9a1755182441504bfcc10c
Nanthini.N.L <nanthini2k24@gmail.com>
2026-07-27 13:37:25 +0530

    Refactor wellness.js for improved structure

--- wellness.js
+++ wellness.js
@@ -1,6 +1,6 @@
 // ===== Mental Wellness page =====
-import { store, addActivity, checkAchievements, computeStats, confetti } from '../utils.js';
-import { breathingExercises, affirmations } from '../dat.js';
+import { store, addActivity, checkAchievements, computeStats, confetti } from './utils.js';
+import { breathingExercises, affirmations } from './dat.js';
 
 let breathTimer = null;
 let breathStep = 0;
